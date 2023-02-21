97a43545746965fb692ab263e4852dd325418722
andika <yuda.yokesen@gmail.com>
2023-02-21 16:55:33 +0700

    friend card

---
+++ src/friend.js
@@ -0,0 +1,64 @@
+import React,{Component} from 'react';  
+import './css/card.css'
+import { Link } from 'react-router-dom';
+import datas from './data/friend.json'
+import alldata from './data/all.json'
+import { Helmet } from 'react-helmet';
+import Footer from './footer';
+
+
+const data = datas.data
+const tambahan = alldata.data
+
+export class FriendCard extends Component {
+
+    generateQuestion() {
+        var datas = data.concat(tambahan)
+        var n = datas.length;
+        console.log('Panjang data', n)
+        var random = Math.floor(Math.random() * n);
+        this.question = datas[random]
+    }
+
+    render()  
+    {  
+      this.generateQuestion()
+      const code = this.question;
+      return (  
+
+            <section className='body-class'>
+
+            <Helmet>
+                <title>Truthordare | CrushCard</title>
+            </Helmet>
+
+            <a>
+            <Link to="/">
+             <img className="back-icon" src={require('./images/icons8-back-arrow-64.png')} alt=""/>
+             </Link>
+            </a>
+
+            <a className='tap-card'>Tap Kartu Untuk Membuka</a>
+            <div className="card-open">
+                <div className="fundo">
+                    <img src={require('./images/12.png')} alt=""/>
+                    <div id="question" className="question-text-card"> {code}
+                    </div>
+                </div>
+                <div className="frente">
+                    <img src={require('./images/11.png')} alt=""/>
+                </div>
+            </div>
+            <Link to="/friend-card">
+                <img className='reload-button' src={require('./images/icons8-reset-100.png')} alt=""/>
+            </Link>
+            <a className='reload-text'>Tekan icon untuk mengganti kartu</a>
+            <Footer/>
+            </section>
+
+            )  
+    }  
+  }  
+    
+
+export default FriendCard  
