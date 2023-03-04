38234335bf3591faf8ee11c6019189298e14b4b2
andika <yuda.yokesen@gmail.com>
2023-03-04 21:14:07 +0700

    iklan smatoo

--- src/App.js
+++ src/App.js
@@ -18,45 +18,46 @@ function App() {
 
   return (
     <ReactPwa
-    test //is to install in localhost, not required
-    suspense={<>
-      Opening  dari-hati Apps
-    </>}
-    config={{
-      swUrl: "/service-worker.js", // sw file in public default is service-worker.js
-      onUpdate: (reg) => {
-        console.log(reg);
-      },
-      onSuccess: (reg) => {
-        console.log(reg);
-      },
-      onError: (reg) => {
-        console.log(reg);
-      },
-      onPrompt:(e) => {
-        if(e.outcome === 'accepted'){
-          console.log('user click on install and accept')
-        }
-        if(e.outcome === 'dismissed'){
-          console.log('user click on install and refuse')
-        }
-      },
-    }} 
+      test //is to install in localhost, not required
+      suspense={<>
+        Opening  dari-hati Apps
+      </>}
+      config={{
+        swUrl: "/service-worker.js", // sw file in public default is service-worker.js
+        onUpdate: (reg) => {
+          console.log(reg);
+        },
+        onSuccess: (reg) => {
+          console.log(reg);
+        },
+        onError: (reg) => {
+          console.log(reg);
+        },
+        onPrompt: (e) => {
+          if (e.outcome === 'accepted') {
+            console.log('user click on install and accept')
+          }
+          if (e.outcome === 'dismissed') {
+            console.log('user click on install and refuse')
+          }
+        },
+      }}
     >
-        <main>
-          <header className='header'>
-            <h3 >Support Developer? <a href='https://saweria.co/titikkomadev' target='__blank'>Disini tempatnya</a></h3>
-          </header>
-            <Switch>
-                <Route path="/" component={Dashboard} exact />
-                <Route path="/crush-card" component={CrushCard} />
-                <Route path="/couple-card" component={CoupleCard} />
-                <Route path="/about-card" component={AboutCard} />
-                <Route path="/friend-card" component={FriendCard} />
-            </Switch>
-        </main>
-        
-       </ReactPwa>
+      <main>
+        <header className='header'>
+          <h3 >Support Developer? <a href='https://saweria.co/titikkomadev' target='__blank'>Disini tempatnya</a></h3>
+        </header>
+        <div id="smt-134641283" style="padding: 0px"></div>
+        <Switch>
+          <Route path="/" component={Dashboard} exact />
+          <Route path="/crush-card" component={CrushCard} />
+          <Route path="/couple-card" component={CoupleCard} />
+          <Route path="/about-card" component={AboutCard} />
+          <Route path="/friend-card" component={FriendCard} />
+        </Switch>
+      </main>
+
+    </ReactPwa>
   );
 
 }
